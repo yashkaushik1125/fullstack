ed5939d59ba2c8fbf7ae296845e0020c366057d9
yashkaushik1125 <118420808+yashkaushik1125@users.noreply.github.com>
2025-07-17 14:14:54 +0530

    JWT,MODELS

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json({limit:'10kb'}))
 app.use(express.urlencoded({extended: true , limit:'10kb'}))
 app.use(express.static('public'))
-//app.use(express.cookieParser())
+app.use(express.cookieParser)
 
 
 
