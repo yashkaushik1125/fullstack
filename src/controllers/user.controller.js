90438cdbffe6c95e44742a449d054896fad24312
yashk31 <118420808+yashkaushik1125@users.noreply.github.com>
2025-07-24 12:24:25 +0000

    office

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,119 +1,129 @@
 import { asynchandler } from "../utils/asyncHandler.js";
-import {ApiError} from '../utils/apiError.js'
-import {User} from '../models/user.model.js'
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
 import { uploadCloudinary } from "../utils/cloudinary.js";
 import { apiResponse } from "../utils/apiResponse.js";
 
+const genrateAccessrefreshToken = async (userID) => {
+  try {
+    const user = await User.findById(userID);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
 
-const genrateAccessrefreshToken = async (userID)=>{
-    try {
-        const user = await User.findById(userID)
-        const accessToken = user.genrateAccessToken()
-        const refreshToken = user.genrateRefreshToken()
+    user.refreshToken = refreshToken;
+    user.accessToken = accessToken;
 
-        user.refreshToken= refreshToken
-        user.accessToken=accessToken
+    await user.save({ validateBeforeSave: false });
 
-        await user.save({validateBeforeSave:false})
+    return { accessToken, refreshToken };
+  } catch (error) {
+    console.log(error);
+    throw new ApiError(505, "something went wrong  Access not genrated", [
+      error,
+    ]);
+  }
+};
 
-        return {accessToken,refreshToken}
+const registerUser = asynchandler(async (req, res) => {
+  console.log(req.body);
+  const { email, fullName, username, password } = req.body;
+  //
 
-    } catch (error) {
-        throw new ApiError(505,'something went wrong  Access not genrated')
-        
-    }
+  if ([fullName, email, password, username].some((e) => e?.trim() === ""))
+    throw new ApiError(400, "all feilds are required");
 
-}
+  const ExistedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (ExistedUser) {
+    throw new ApiError(409, "User Exists Already");
+  }
+  const localAvtarPath = req.files?.avtar[0]?.path;
+  const localCoverPath = req.files?.coverImage[0]?.path;
+  console.log(localAvtarPath);
 
+  if (!localAvtarPath) throw new ApiError(404, "Avtar file is required");
 
+  const avtar = await uploadCloudinary(localAvtarPath);
+  const coverImage = await uploadCloudinary(localCoverPath);
 
-const registerUser = asynchandler(async (req, res)=>{
-    const {email , fullName , username , password }=req.body
-   //
-    console.log(req.body)
+  if (!avtar) throw new ApiError(404, "Avtar is required");
 
-    if([fullName,email,password,username].some((e)=>e?.trim()===""))
-        throw new ApiError (400,"all feilds are required")
-
-  const ExistedUser= await User.findOne({
-        $or:[{username} , {email}]
-    })
-    if(ExistedUser){ 
-        throw new ApiError(409,"User Exists Already");
-       }
-    const localAvtarPath = req.files?.avtar[0]?.path;
-    const localCoverPath = req.files?.coverImage[0]?.path;
-    console.log(localAvtarPath);
-    
-
-    if(!localAvtarPath) throw new ApiError(404,'Avtar file is required')
-        
-   const avtar= await uploadCloudinary(localAvtarPath)
-   const coverImage = await  uploadCloudinary(localCoverPath)
-
-   if(!avtar) throw new ApiError(404, "Avtar is required")
-
-  const myUser= await User.create({
-        fullName,
-        avtar:avtar.url,
-        coverImage:coverImage?.url || "",
-        email,
-        password,
-        username:username
-    })
-   const createdUser = await User.findById(myUser._id).select(
+  const myUser = await User.create({
+    fullName,
+    avtar: avtar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username,
+  });
+  const createdUser = await User.findById(myUser._id).select(
     "-password -refreshToken"
-   )
-   if(!createdUser) throw new ApiError(500,'User not created , something went wrong')
+  );
+  if (!createdUser)
+    throw new ApiError(500, "User not created , something went wrong");
 
-    return res.status(200).json(new apiResponse(200,createdUser,'user created succesfully'))
-    
-})
-
+  return res
+    .status(200)
+    .json(new apiResponse(200, createdUser, "user created succesfully"));
+});
 
 const loginUser = asynchandler(async (req, res) => {
+  console.log(req.body);
+  const { email, password, username } = req.body;
+  if (!username && !email)
+    throw new ApiError(400, "Email or username is required");
 
-    const { email, password , username } = req.body;
-    if(!username||!email)
-        throw new ApiError(400,'Email or username is required')
+  const isPresent = await User.findOne({ $or: [{ email }, { username }] });
+  if (!isPresent) throw new ApiError(404, "user doesnot exist");
 
-    const isPresent = await User.findOne({$or:[{email},{username}]})
-    if(!isPresent) throw new ApiError(404,'user doesnot exist')
+  const isPasswordCorrect = await isPresent.isPasswordCorrect(password);
+  if (!isPasswordCorrect) throw new ApiError(404, "wrong Password");
 
-    const isPasswordCorrect= await isPresent.isPasswordCorrect(password)
-    if(!isPasswordCorrect) throw new ApiError(404, 'wrong Password')
-        
-    const { accessToken , refreshToken } =await genrateAccessrefreshToken(isPresent._id)
+  const { accessToken, refreshToken } = await genrateAccessrefreshToken(
+    isPresent._id
+  );
+  console.log("working till 1");
+  const loggedInUser = User.findById(isPresent._id).select(
+    "-password -refreshToken"
+  );
 
-    const loggedInUser = User.findById(isPresent._id).select("-password -refreshToken")
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  const responseObj = {
+    isPresent: loggedInUser,
+    accessToken,
+    refreshToken,
+  };
+  console.log("wjhyorking till 2");
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new apiResponse(200, loggedInUser, "UserLoggedIn Success"));
+});
 
-    const options = {
-        httpOnly= true,
-        secure = true
-    }
-    return res.status(200).cookie("accessToken",accessToken, options).cookie("refrehToken",refreshToken,options)
-    .json(new apiResponse(200,{
-        isPresent: loggedInUser accessToken, refreshToken
+const logoutUser = asynchandler(async (req, res) => {
+  await User.findByIdAndUpdate(req.user._id, {
+    $set: {
+      refreshToken: undefined,
     },
-    "UserLoggedIn Success" 
-))
+  });
 
-    
-})
-
-const logoutUser = asynchandler(async (req, res)=>{
-   await User.findByIdAndUpdate(req.user._id,{
-        $set:{
-            refreshToken: undefined
-        },
-        
-    })
-
-     const options = {
-        httpOnly= true,
-        secure = true
-    }
-    return res.status(200).clearCookie("accessToken",options).clearCookie("refreshToken",options).json(new apiResponse(200,'Loggedut Success'))
-})
-
-export {registerUser, loginUser,logoutUser};
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  try {
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .send(new apiResponse(200, "Loggedut Success"));
+  } catch (error) {
+    console.log(error);
+  }
+});
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,26 +1,31 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
-import {upload } from '../middlewares/multer.middleware.js'
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
-router.route('/register').post(
-    upload.fields([
-        {
-            name: 'avtar',
-            maxCount:1
-        },
-        {
-            name:'coverImage',
-            maxCount:1
-        }
-    ]),
-    registerUser)
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avtar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-router.route('/login').post(loginUser)
+router.route("/login").post(loginUser);
 
 ////////////
-router.route('/logout').post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 
-export default router
+export default router;
