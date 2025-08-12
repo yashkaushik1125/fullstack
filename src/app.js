3a7c9161be4fd94f9272155043455d996f1d26b5
yashkaushik1125 <118420808+yashkaushik1125@users.noreply.github.com>
2025-08-13 00:44:31 +0530

    2ndenvupdate

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser'
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials:true
     
 }))
